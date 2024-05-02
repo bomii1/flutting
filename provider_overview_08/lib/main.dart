b516ef11209903a170e9eee77050608cbd7672c9
bomii1 <202103076@hufs.ac.kr>
2024-05-02 19:14:21 +0900

    provider_overview08

--- provider_overview_08/lib/main.dart
+++ provider_overview_08/lib/main.dart
@@ -42,6 +42,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Provider 08'),
       ),
       body: Consumer<Dog>(
+        // 빌더함수는 3개의 argument 를 가짐 -> 빌더 콘테스트/provided argument/빌드를 최적화할 때 사용하는 optional argument 인 child
+        // BuilderContext 사용x -> _
+        // 최적화에 필요한 child 사용x -> __
+        // child -> 다시 리빌드 되지 않는 부분을 의미
         builder: (BuildContext context, Dog dog, Widget? child) {
           return Center(
             child: Column(
@@ -61,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
           );
         },
         child: const Text(
-          'I like dogs very much',
+          'I like dogs very much', // 리빌드하지 않아도 되는 -> 리빌드하고 싶지 않음
           style: TextStyle(fontSize: 20.0),
         ),
       ),
@@ -118,3 +122,15 @@ class Age extends StatelessWidget {
     );
   }
 }
+
+
+/* 
+Consumer
+-> 새로운 위젯에서 provider.of 를 호출하고 위젯의 빌더에 위임한다
+-> 빌더는 널이 아니어야하고 값이 변할때마다 빌더가 호출되어야 한다
+
+컨슈머를 사용하면 리빌드할 필요가 없는 것도 리빌드 되는 것 아닌가?
+-> 대비해서 널러블 타입의 child argument 가 존재함
+
+
+*/
