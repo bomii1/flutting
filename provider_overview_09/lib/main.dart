6dda976ccf0793ad40395d04863188fd725185ea
bomii1 <202103076@hufs.ac.kr>
2024-05-03 00:42:19 +0900

    provider_overview09

---
+++ provider_overview_09/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Provider 09',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class Foo with ChangeNotifier {
+  String value = 'Foo';
+
+  void changeValue() {
+    value = value == 'Foo' ? 'Bar' : 'Foo';
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider 09'),
+      ),
+      body: ChangeNotifierProvider<Foo>(
+        create: (_) => Foo(),
+        child: Consumer<Foo>(
+          builder: (BuildContext context, Foo foo, Widget? _) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    foo.value,
+                    style: const TextStyle(fontSize: 40),
+                  ),
+                  const SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () => foo.changeValue(),
+                    child: const Text(
+                      'Change Value',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
